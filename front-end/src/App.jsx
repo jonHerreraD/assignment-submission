11b89e00cd8c0cd6d3aa21fa44acdee5095799f6
Jonathan <diazjonathan3009@gmail.com>
2025-01-26 12:20:43 -0600

    Add login form functionality
    Now we can send requests when attempting to login from our login form through the login button

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -15,34 +15,6 @@ function App() {
   const [count, setCount] = useState(0)
   const [jwt, setJwt] = useLocalState("", "jwt");
 
-  /*
-  useEffect(() => {
-
-    if(!jwt){
-      const requestBody = {
-        "username": "jon",
-        "password": "jonsnow"
-      }
-    
-      fetch('api/auth/login',{
-        headers : {
-          "Content-Type": "application/json"
-        },
-        method : "post",
-        body: JSON.stringify(requestBody)
-      })
-      .then((response) => Promise.all([response.json(), response.headers]))
-      .then(([body, headers])=>{
-        setJwt(headers.get("authorization"));
-      });
-    }
-  }, []);
-  
-  useEffect(() =>{
-    console.log(`JWT is: ${jwt}` );
-  },[jwt]);
-
-*/
   return (
     <Routes>
       <Route path='/dashboard' element={
